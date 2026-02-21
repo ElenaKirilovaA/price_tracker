7ba77d925e679dd21ed98e2edede4fd4662cbbd7
Elena Kirilova <elenakirilova@Elenas-MacBook-Pro.local>
2026-02-21 18:08:59 +0200

    urls alert order

--- alert/urls.py
+++ alert/urls.py
@@ -1,16 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 
 from alert import views
 
 app_name = 'alert'
 
+paths = [
+        path('info/', views.archive_alert_info, name='info_archive'),
+        path('check/', views.check_alerts, name='check'),
+        path('edit/', views.alert_edit, name='edit'),
+        path('delete/', views.alert_delete, name='delete'),
+]
 urlpatterns = [
         path('list/', views.DisplayActiveAlerts.as_view(), name='alert_list'),
         path('create/', views.alert_create, name='create'),
-        path('<int:archived_id>/info/', views.archive_alert_info, name='info_archive'),
-        path('<int:product_id>/check/', views.check_alerts, name='check'),
-        path('<int:alert_id>/edit/', views.alert_edit, name='edit'),
-        path('<int:alert_id>/delete/', views.alert_delete, name='delete'),
+        path('<int:archived_id>/', include(paths)),
         path('past-alerts/', views.DisplayArchivedAlerts.as_view(), name='history_list')
     ]
 
