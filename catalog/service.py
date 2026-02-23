d15781cf16f3c5640987964cd8f9c382850b6ba1
Elena Kirilova <elenakirilova@Elenas-MacBook-Pro.local>
2026-02-23 16:22:02 +0200

    set for tags to create optimization

--- catalog/service.py
+++ catalog/service.py
@@ -3,11 +3,10 @@ from catalog.models import Tag
 
 def create_tags(tags_to_create: set) -> list or None:
     tag_titles_already_exist = set(Tag.objects
-                               .filter(title__=tags_to_create)
-                               .values_list('title', flat=True))  #  value_list flat=True -> ['cool', 'fun'] ;
-                                                                         #  flat=False [('cool'), ('fun')];
-                                                                         #  without valUe_list -> [<Tag: cool>, <Tag: fun>]
-    new_tags = [Tag(title=t) for t in tags_to_create if t not in tag_titles_already_exist]  # bulk_create needs [obj]
+                               .filter(title__in=tags_to_create)
+                               .values_list('title', flat=True))
+
+    new_tags = [Tag(title=t) for t in tags_to_create if t not in tag_titles_already_exist]
     Tag.objects.bulk_create(new_tags)
 
     return new_tags
