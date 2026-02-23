acb252b986651b18d7f287c940c0fe594a11f0f6
Elena Kirilova <elenakirilova@Elenas-MacBook-Pro.local>
2026-02-23 12:57:10 +0200

    delete validator.py

--- common/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.core.validators import validate_email
-from django.utils.deconstruct import deconstructible
-
-
-@deconstructible
-class TargetPriceValidator:
-    pass
