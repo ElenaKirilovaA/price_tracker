ad1abb1f67b78dd7b39de8d0c70812125999c235
ElenaKIrilovaA <kirilovae@rocketmail.com>
2026-03-15 14:54:35 +0200

    smtp

--- price_tracker/settings.py
+++ price_tracker/settings.py
@@ -129,8 +129,13 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'no-reply@pricetracker.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mailjet.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASS = os.getenv('EMAIL_HOST_PASS')
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = 'xavor64700@flosek.com'
+
 
 AUTH_USER_MODEL = 'accounts.AppUser'
 LOGIN_REDIRECT_URL = 'accounts:dashboard'
