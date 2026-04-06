600cc1b422f690825da0babf983a864b3eb8782b
ElenaKIrilovaA <kirilovae@rocketmail.com>
2026-04-06 15:27:01 +0300

    change start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Starting celery worker"
-celery -A price_tracker worker --loglevel=info &
-echo "Starting celery beat"
-celery -A price_tracker beat --loglevel=info &
-echo "Starting unicorn"
+#celery -A price_tracker worker --loglevel=info &
+#celery -A price_tracker beat --loglevel=info &
 gunicorn price_tracker.wsgi:application --bind=0.0.0.0:8000
