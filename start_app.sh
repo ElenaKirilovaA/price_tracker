5e8c7831941dcdb757360a92fa31da78ddb6bcfa
ElenaKIrilovaA <kirilovae@rocketmail.com>
2026-04-06 12:39:55 +0300

    fix startapp

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo "Starting celery worker"
 celery -A price_tracker worker --loglevel=info &
+echo "Starting celery beat"
 celery -A price_tracker beat --loglevel=info &
-gunicorn price_tracker.wsgi:application --bind=0.0.0.0:$PORT
+echo "Starting unicorn"
+gunicorn price_tracker.wsgi:application --bind=0.0.0.0:8000
