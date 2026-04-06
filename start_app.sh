6d2e90fbafe3fe456865dd9d384d3aa42263e6bc
ElenaKIrilovaA <kirilovae@rocketmail.com>
2026-04-06 16:29:05 +0300

    gnicorn settings

--- start_app.sh
+++ start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-celery -A price_tracker worker --loglevel=info &
-celery -A price_tracker beat --loglevel=info &
+#celery -A price_tracker worker --loglevel=info &
+#celery -A price_tracker beat --loglevel=info &
 gunicorn price_tracker.wsgi:application --bind=0.0.0.0:8000
