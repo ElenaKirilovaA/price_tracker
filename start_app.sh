9a09bb11f0befc742cf045cca4b30f3d8b6276c2
ElenaKIrilovaA <kirilovae@rocketmail.com>
2026-04-07 01:11:52 +0300

    html fixes

--- start_app.sh
+++ start_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 celery -A price_tracker worker --loglevel=info --pool=solo &
-celery -A price_tracker beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+celery -A price_tracker beat --loglevel=info &
 
 
 exec gunicorn price_tracker.wsgi:application --bind=0.0.0.0:$PORT
