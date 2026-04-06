6812f3f39bf10e17a918b13a51ba76b9b427b1ad
ElenaKIrilovaA <kirilovae@rocketmail.com>
2026-04-06 16:57:50 +0300

    start app

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-elery -A price_tracker worker -l info &
-celery -A price_tracker beat -l info %
-gunicorn price_tracker.wsgi:application --bind=0.0.0.0:8000
+# Стартираме Celery worker и beat като background процеси
+celery -A price_tracker worker --loglevel=info --pool=solo &
+celery -A price_tracker beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+
+# Стартираме Django през Gunicorn (foreground)
+exec gunicorn price_tracker.wsgi:application --bind=0.0.0.0:8000
