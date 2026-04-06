d34d2e99bb8468a861719d2d294f48588624b73e
ElenaKIrilovaA <kirilovae@rocketmail.com>
2026-04-06 16:46:57 +0300

    fixes

--- start_app.sh
+++ start_app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#celery -A price_tracker worker --loglevel=info &
-#celery -A price_tracker beat --loglevel=info &
+
+elery -A price_tracker worker -l info &
+celery -A price_tracker beat -l info %
 gunicorn price_tracker.wsgi:application --bind=0.0.0.0:8000
